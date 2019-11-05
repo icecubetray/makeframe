3cf01ca4e479ecb254680c5688b786404bc0c3d3
icecubetray <icecubetray@users.noreply.github.com>
2019-11-05 21:15:47 +0100

    Rename rules.mk to targets.mk

--- bootstrap.mk
+++ bootstrap.mk
@@ -3,6 +3,6 @@ DIR_SELF := $(shell dirname "$(realpath $(lastword $(MAKEFILE_LIST)))" | sed 's/
 include \
 	$(DIR_SELF)/src/globals.mk \
 	$(DIR_SELF)/src/functions.mk \
-	$(DIR_SELF)/src/rules.mk \
+	$(DIR_SELF)/src/targets.mk \
 	$(DIR_SELF)/src/phonies.mk \
 	$(DIR_SELF)/src/sanity.mk
