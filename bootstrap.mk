000a73569f94ab9c8c03af78c6556360c6e32006
icecubetray <icecubetray@users.noreply.github.com>
2019-11-05 22:05:08 +0100

    Move sanity back up

--- bootstrap.mk
+++ bootstrap.mk
@@ -1,8 +1,8 @@
 DIR_SELF := $(shell dirname "$(realpath $(lastword $(MAKEFILE_LIST)))" | sed 's/ /\\ /g')
 
 include \
+	$(DIR_SELF)/src/sanity.mk \
 	$(DIR_SELF)/src/globals.mk \
 	$(DIR_SELF)/src/functions.mk \
 	$(DIR_SELF)/src/targets.mk \
-	$(DIR_SELF)/src/phonies.mk \
-	$(DIR_SELF)/src/sanity.mk
+	$(DIR_SELF)/src/phonies.mk
